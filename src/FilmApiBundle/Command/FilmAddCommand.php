4651ee4bc808d15779808bcf7439dc13f28f0ec5
aaidettaa <aida.aso@gmail.com>
2016-03-08 22:09:14 +0100

    implement commands - init

---
+++ src/FilmApiBundle/Command/FilmAddCommand.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace FilmApiBundle\Command;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class FilmAddCommand extends Command
+{
+
+	protected function configure()
+	{
+		$this
+			->setName('film:add')
+			->setDescription('Greet someone')
+			->addArgument(
+				'first_number',
+				InputArgument::REQUIRED,
+				'Enter the first number'
+			)
+			->addArgument(
+				'second_number',
+				InputArgument::REQUIRED,
+				'Enter the second number'
+			)
+		;
+	}
+
+	protected function execute(InputInterface $input, OutputInterface $output)
+	{
+		$firs_number = $input->getArgument('first_number');
+		$second_number = $input->getArgument('second_number');
+
+		$obj_calculadora = $this->getContainer()->get('calculadora');
+		$result = $obj_calculadora->mult($firs_number,$second_number);
+		$output->writeln($result);
+	}
+}
