fe7c531a47600419c1a36f6adeae345b17f9598a
Marcos <velozmarkdrea@gmail.com>
2016-03-09 21:29:48 +0100

    Building add film service

--- src/FilmApiBundle/Service/AddFilmUseCase.php
+++ src/FilmApiBundle/Service/AddFilmUseCase.php
@@ -2,21 +2,23 @@
 
 namespace FilmApiBundle\Service;
 
-
 use FilmApiBundle\Entity\Film;
+use FilmApiBundle\Repository\FilmRepository;
 
 final class AddFilmUseCase
 {
+    private $films_repository;
 
-    private $film;
-
-    public function __construct($a_name, $an_year, $a_date, $an_url)
+    public function __construct(FilmRepository $a_films_repository)
     {
-        $this->film = Film::register($a_name, $an_year, $a_date, $an_url);
+        $this->films_repository = $a_films_repository;
     }
 
     public function __invoke()
     {
+        //$film = Film::register($a_name, $an_year, $a_date, $an_url);
+
+        $this->films_repository->listFilms();
 
     }
 }
