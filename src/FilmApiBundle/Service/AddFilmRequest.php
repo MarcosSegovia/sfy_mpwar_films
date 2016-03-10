2374f300558cf9359a0248a03d1e6140e2941ae4
Marcos <velozmarkdrea@gmail.com>
2016-03-10 19:59:39 +0100

    Defining Request DTO for the AddFilm Service

--- src/FilmApiBundle/Service/AddFilmRequest.php
+++ src/FilmApiBundle/Service/AddFilmRequest.php
@@ -2,7 +2,41 @@
 
 namespace FilmApiBundle\Service;
 
-class AddFilmRequest
+final class AddFilmRequest
 {
+	private $name;
 
+	private $year;
+
+	private $date;
+
+	private $url;
+
+	public function __construct($a_name, $a_year, $a_date, $an_url)
+	{
+		$this->name = $a_name;
+		$this->year = $a_year;
+		$this->date = $a_date;
+		$this->url = $an_url;
+	}
+
+	public function name()
+	{
+		return $this->name;
+	}
+
+	public function year()
+	{
+		return $this->year;
+	}
+
+	public function date()
+	{
+		return $this->date;
+	}
+
+	public function url()
+	{
+		return $this->url;
+	}
 }
