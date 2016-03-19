adfeb812b4d76a03a93f3365320bf9c021afca2e
Marcos Segovia <velozmarkdrea@gmail.com>
2016-03-19 19:29:51 +0100

    Defining DeleteFilm as a service and calling through the FilmDeleteController

---
+++ src/FilmApiBundle/Controller/FilmDeleteController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace FilmApiBundle\Controller;
+
+use FilmApiBundle\Service\DeleteFilmRequest;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+
+class FilmDeleteController extends Controller
+{
+    /**
+     * @Route("/film/delete", name="homepage")
+     */
+    public function indexAction(Request $request)
+    {
+        $id   = $request->get('id');
+
+        $remove_film_service = $this->get('delete_film_use_case');
+        $new_delete_film_request = new DeleteFilmRequest($id);
+        $remove_film_service->__invoke($new_delete_film_request);
+
+        return new JsonResponse(array('message' => 'OK'));
+    }
+}
