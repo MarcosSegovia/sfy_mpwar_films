6c5a5ef3b912dc0c3b2e8ce6d4cabafe21ec89c5
Marcos Segovia <velozmarkdrea@gmail.com>
2016-03-19 19:09:24 +0100

    Setting Up JMSSerializerBundle in order to serialize the array of Films and retrieve in Json format

--- src/FilmApiBundle/Controller/FilmListController.php
+++ src/FilmApiBundle/Controller/FilmListController.php
@@ -4,23 +4,21 @@ namespace FilmApiBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
 
 class FilmListController extends Controller
 {
     /**
      * @Route("/film/list", name="film-list")
      */
-    public function indexAction(Request $request)
+    public function indexAction()
     {
-        $name = $request->request->get('name');
-        $year = $request->request->get('year');
-        $date = $request->request->get('date');
-        $url = $request->request->get('url');
-
-        $obj_ListfilmUsecase = $this->getContainer()->get('ListfilmUsecase');
-        $result = $obj_ListfilmUsecase();
-
-        return new JsonResponse(array('list' => $result));
+        $list_films_service = $this->get('list_films_use_case');
+        $result = $list_films_service->__invoke();
+        $serializedEntities = $this->container->get('serializer')->serialize($result, 'json');
+        
+        return new Response($serializedEntities);
     }
+
 }
