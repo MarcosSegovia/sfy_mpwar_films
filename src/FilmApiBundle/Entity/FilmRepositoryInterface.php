4a6a1edc112e6b1b19389569355be9b18449216f
Marcos Segovia <velozmarkdrea@gmail.com>
2016-03-19 18:26:24 +0100

    Making FilmRepository to implement our own interface, so the client can couple just to the interface

---
+++ src/FilmApiBundle/Entity/FilmRepositoryInterface.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace FilmApiBundle\Entity;
+
+interface FilmRepositoryInterface
+{
+	public function findById($a_raw_id);
+	public function listFilms();
+	public function updateFilms();
+}
--- src/FilmApiBundle/Repository/FilmRepository.php
+++ src/FilmApiBundle/Repository/FilmRepository.php
@@ -2,13 +2,24 @@
 
 namespace FilmApiBundle\Repository;
 
-use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\EntityRepository;
+use FilmApiBundle\Entity\FilmRepositoryInterface;
 
-class FilmRepository extends EntityRepository
+class FilmRepository extends EntityRepository implements FilmRepositoryInterface
 {
+
+	public function findById($a_raw_id)
+	{
+		return $this->find($a_raw_id);
+	}
+
+	public function updateFilms()
+	{
+		$this->_em->flush();
+	}
+
 	public function listFilms()
 	{
-		echo "co";
+
 	}
 }
