ecb380b52fa49e6a425c8a2e2371c44955c1bce1
Marcos Segovia <velozmarkdrea@gmail.com>
2016-03-19 19:23:35 +0100

    Building DeleteFilm Service and deleting the previous RemoveFilm one

---
+++ src/FilmApiBundle/Service/DeleteFilm.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace FilmApiBundle\Service;
+
+use Doctrine\ORM\EntityManager;
+use Symfony\Component\EventDispatcher\EventDispatcherInterface;
+
+final class DeleteFilm
+{
+	private $entity_manager;
+	private $event_dispatcher;
+
+	public function __construct(
+		EntityManager $an_em,
+		EventDispatcherInterface $an_event_dispatcher
+	)
+	{
+		$this->entity_manager   = $an_em;
+		$this->event_dispatcher = $an_event_dispatcher;
+	}
+
+	public function __invoke(DeleteFilmRequest $request)
+	{
+		$the_film_to_delete = $this->entity_manager->find('Film', $request->id());
+		$this->entity_manager->remove($the_film_to_delete);
+		$this->entity_manager->flush();
+	}
+
+}
---
+++ src/FilmApiBundle/Service/DeleteFilmRequest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace FilmApiBundle\Service;
+
+class DeleteFilmRequest
+{
+	private $id;
+
+	public function __construct($a_raw_id)
+	{
+		$this->id = $a_raw_id;
+	}
+
+	public function id()
+	{
+		return $this->id;
+	}
+
+}
