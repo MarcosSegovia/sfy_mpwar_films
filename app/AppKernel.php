cd6de0223380e9ebf35b7cd89af0b4a3fe458e30
Marcos Segovia <velozmarkdrea@gmail.com>
2016-03-20 12:36:20 +0100

    Registering the CacheBundle within the framework

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,6 +17,7 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new JMS\SerializerBundle\JMSSerializerBundle(),
             new FilmApiBundle\FilmApiBundle(),
+            new CacheBundle\CacheBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
