d86653eaf8751bad5dc199a22faba10b5ba25f29
Marcos Segovia <velozmarkdrea@gmail.com>
2016-03-20 16:54:50 +0100

    Making CachedFilmRepository decorator to handle the events and rebuild cache when catch any of the events

--- src/CacheBundle/Repository/CachedFilmRepository.php
+++ src/CacheBundle/Repository/CachedFilmRepository.php
@@ -7,10 +7,12 @@ use FilmApiBundle\Entity\FilmRepositoryInterface;
 class CachedFilmRepository implements FilmRepositoryInterface
 {
 	private $film_repository;
+	private $array_cached_films;
 
 	public function __construct(FilmRepositoryInterface $a_film_repository)
 	{
-		$this->film_repository = $a_film_repository;
+		$this->film_repository    = $a_film_repository;
+		$this->array_cached_films = NULL;
 	}
 
 	public function findById($a_raw_id)
@@ -20,11 +22,32 @@ class CachedFilmRepository implements FilmRepositoryInterface
 
 	public function listFilms()
 	{
-		return $this->film_repository->listFilms();
+		dump($this->array_cached_films);
+		if ($this->array_cached_films === NULL)
+		{
+			$this->array_cached_films = $this->film_repository->listFilms();
+		}
+
+		return $this->array_cached_films;
 	}
 
 	public function updateFilms()
 	{
 		$this->film_repository->updateFilms();
+	}
+
+	public function onFilmAdded()
+	{
+		$this->array_cached_films = $this->film_repository->listFilms();
+	}
+
+	public function onFilmEdited()
+	{
+		$this->array_cached_films = $this->film_repository->listFilms();
+	}
+
+	public function onFilmDeleted()
+	{
+		$this->array_cached_films = $this->film_repository->listFilms();
 	}
 }
