fe24d9d1c50e5c607c72b5355b1667fbb05acb24
aaidettaa <aida.aso@gmail.com>
2016-03-28 21:05:41 +0200

    create form

---
+++ src/FilmApiBundle/Form/FilmType.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace FilmApiBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class FilmType extends AbstractType
+{
+    /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name')
+            ->add('year')
+            ->add('date', 'datetime')
+            ->add('url')
+        ;
+    }
+    
+    /**
+     * @param OptionsResolver $resolver
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'FilmApiBundle\Entity\Film',
+            'csrf_protection'   => false
+        ));
+    }
+}
