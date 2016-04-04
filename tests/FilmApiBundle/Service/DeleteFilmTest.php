37d305df16cf8f9b204da8cfa5c3183481aaa01b
aaidettaa <aida.aso@gmail.com>
2016-04-04 22:01:48 +0200

    edit tests

--- tests/FilmApiBundle/Service/DeleteFilmTest.php
+++ tests/FilmApiBundle/Service/DeleteFilmTest.php
@@ -4,17 +4,28 @@
 namespace Tests\FilmApiBundle\Service;
 
 
-use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
-use Symfony\Component\DependencyInjection\ContainerInterface;
+use FilmApiBundle\Service\DeleteFilmRequest;
 
 class DeleteFilmTest extends BaseFilmServiceTest
 {
 
 
-
     public function testDeleteFilm()
     {
 
+        $list_films_service = $this->container->get('list_films_use_case');
+        $result = $list_films_service->__invoke();
+
+        $remove_film_service = $this->container->get('delete_film_use_case');
+        $new_delete_film_request = new DeleteFilmRequest(1);
+        $remove_film_service->__invoke($new_delete_film_request);
+
+
+        $result2 = $list_films_service->__invoke();
+
+        $expected = count($result) - 1;
+        $actual = count($result2);
+        $this->assertEquals($expected, $actual);
 
     }
 
